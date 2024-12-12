d0e880ecbf540c2f58060a297776edb328b7beb2
jeff1728 <jschafuelan@espe.edu.ec>
2024-12-12 11:34:37 -0500

    form

--- contact.js
+++ contact.js
@@ -1,12 +1,13 @@
 class ContactoForm extends HTMLElement {
     constructor() {
         super();
-        this.attachShadow({ mode: 'open' });
+    }
+    connectedCallback(){
+        let shadowRoot =  this.attachShadow({ mode: 'open' });
 
         // Select the template from the document
         const templateContent = document.getElementById('formulario').content;
         this.shadowRoot.appendChild(templateContent.cloneNode(true));
-
         this.shadowRoot.querySelector('form').addEventListener('submit', (event) => {
             if (!event.target.checkValidity()) {
                 event.preventDefault(); // Evitar el envío
@@ -14,7 +15,11 @@ class ContactoForm extends HTMLElement {
                 event.target.classList.add('was-validated'); // Aplicar estilos de Bootstrap para campos inválidos
             }
         });
+
     }
-}
+
+        
+    }
+
 
 customElements.define('contacto-form', ContactoForm);
